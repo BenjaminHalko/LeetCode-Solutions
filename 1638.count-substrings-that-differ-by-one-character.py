ddfce31bf9c77fe990e887db87543777f7669925
BenjaminHalko <benjaminhalko@hotmail.com>
2022-06-08 15:38:14 -0700

    Updated

---
+++ 1638.count-substrings-that-differ-by-one-character.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1638 lang=python3
+#
+# [1638] Count Substrings That Differ by One Character
+#
+
+# @lc code=start
+class Solution:
+    def countSubstrings(self, s: str, t: str) -> int:
+        
+# @lc code=end
+
