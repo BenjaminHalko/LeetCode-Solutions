fc709072ebd465599b9db3a77fd3c767ffb4e57b
Benjamin <73490201+BenjaminHalko@users.noreply.github.com>
2022-06-10 14:17:45 -0700

    Time: 74 ms (70.54%), Space: 14 MB (15.60%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,5 +1,5 @@
-class Solution(object):
-    def isPalindrome(self, x):
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
         if(x < 0 or (x % 10 == 0 and x != 0)): return False
         x = str(x)
         return x == x[::-1]
