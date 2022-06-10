c17bb8ca8d04ec7fee260bd1e3962c50edb16a3a
Benjamin <73490201+BenjaminHalko@users.noreply.github.com>
2022-06-10 14:16:37 -0700

    Time: 132 ms (14.26%), Space: 14 MB (42.73%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1, l2):
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         l1.val = str(l1.val)
         l2.val = str(l2.val)
         while l1.next != None:
