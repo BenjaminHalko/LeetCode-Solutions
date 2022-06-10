30857f524776291ec78c4c63fdea69fa25910ed8
Benjamin <73490201+BenjaminHalko@users.noreply.github.com>
2022-06-10 14:14:25 -0700

    Time: 100 ms (33.61%), Space: 14 MB (99.79%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,8 +1,7 @@
-class Solution(object):
-    def canConstruct(self, ransomNote, magazine):
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         for l in ransomNote:
             if l not in magazine: return False
             x = magazine.index(l)
             magazine = magazine[:x]+magazine[x+1:]
         return True
-        
