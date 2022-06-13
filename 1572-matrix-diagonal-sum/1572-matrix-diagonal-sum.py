63940c0179e628256a0f1f57c667582a95b9a3ac
Benjamin <73490201+BenjaminHalko@users.noreply.github.com>
2022-06-13 15:00:38 -0700

    Time: 144 ms (57.55%), Space: 14.1 MB (93.61%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -3,7 +3,6 @@ class Solution:
         total = 0
         x = len(mat)
         for i in range(x):
-            print(mat[i][i],mat[x-i-1])
             total += mat[i][i]
             if x-1-i != i: total += mat[x-1-i][i]
         return total
