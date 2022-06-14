12538437af32c1abd160c3b804044e925e6c0d99
Benjamin <73490201+BenjaminHalko@users.noreply.github.com>
2022-06-14 14:22:11 -0700

    Time: 108 ms (84.63%), Space: 14.8 MB (36.11%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -5,27 +5,29 @@ class Solution:
         if s == t: return t
         
         sub = defaultdict(int)
-        for i in t:
-            sub[i] += 1
+        for i in t: sub[i] += 1
         
         start = 0
         found = defaultdict(int)
-        allfound = ""
+        foundnum = 0
+        
         result = ""
         minFound = -1
+        
         for end, letter in enumerate(s):
             if letter in sub:
                 found[letter] += 1
-                if allfound.count(letter) < sub[letter]:
-                    allfound += letter
+                if found[letter] <= sub[letter]:
+                    foundnum += 1
                 
             while start < end and (s[start] not in t or found[s[start]] > sub[s[start]]):
-                if found[letter] >= sub[letter]:
-                    allfound.replace(s[start],"",1)
-                if s[start] in sub: found[s[start]] -= 1
+                if s[start] in sub:
+                    if found[letter] < sub[letter]:
+                        foundnum -= 1
+                    found[s[start]] -= 1
                 start += 1
-            
-            if len(allfound) == target and (end-start <= minFound or minFound == -1):
+
+            if foundnum >= target and (end-start <= minFound or minFound == -1):
                 minFound = end-start
                 result = s[start:][:end-start+1]
         return result
