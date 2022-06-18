2940ae948b44ea796584367ec9c6f12d8d691ec2
Benjamin <73490201+BenjaminHalko@users.noreply.github.com>
2022-06-18 13:07:25 -0700

    Time: 71 ms (98.95%), Space: 13.9 MB (34.12%) - LeetHub

--- 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.py
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.py
@@ -3,9 +3,9 @@ class Solution:
         if income == 0: return 0
         
         result = min(brackets[0][0],income) * brackets[0][1] * 0.01
-        if brackets[0][0] > income: return result
-        for i, tax in enumerate(brackets[1:]):
-            result += (min(tax[0],income) - brackets[i][0]) * tax[1] * 0.01
-            if tax[0] > income: break
         
+        for i, tax in enumerate(brackets[1:]):
+            if brackets[i][0] > income: break
+            result += (min(tax[0],income) - brackets[i][0]) * tax[1] * 0.01
+            
         return result
