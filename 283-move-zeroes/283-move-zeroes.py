add1afdac95aef2e2c71b69690e9632e130b32a4
Benjamin <73490201+BenjaminHalko@users.noreply.github.com>
2022-06-18 15:54:08 -0700

    Time: 277 ms (40.67%), Space: 15.6 MB (64.49%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -1,5 +1,3 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        for i in range(nums.count(0)):
-            nums.remove(0)
-            nums.append(0)
+        nums.sort(key=lambda x: x == 0)
