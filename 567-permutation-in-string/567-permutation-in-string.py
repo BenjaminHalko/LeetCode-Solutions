2cf131dc248cf0a773f01e36604828b11b08cb5d
Benjamin <73490201+BenjaminHalko@users.noreply.github.com>
2022-07-25 14:22:10 -0700

    Time: 105 ms (69.24%), Space: 13.9 MB (68.06%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,18 +1,17 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        numsOriginal = Counter(s1)
-        nums = numsOriginal.copy()
+        nums = Counter(s1)
         start = 0
         
         for i,letter in enumerate(s2):
             if letter not in nums:
-                nums = numsOriginal.copy()
+                nums = Counter(s1)
                 start = i+1
             else:
                 nums[letter] -= 1
                 if nums[letter] == 0 and max(nums.values()) == 0: return True
                 while nums[letter] < 0:
-                    if s2[start] in nums: nums[s2[start]] = min(nums[s2[start]]+1,numsOriginal[s2[start]])
+                    if s2[start] in nums: nums[s2[start]] += 1
                     start += 1
         
         return False
